5b52af468941fe68ac0ab03aa41ead62469a99df
mariana296 <mariana2906garcia@gmail.com>
2025-01-26 21:22:59 -0500

    Version 5: Crear el ciclo del triqui

--- OneDrive/Documentos/triqui/Triqui.py
+++ OneDrive/Documentos/triqui/Triqui.py
@@ -9,3 +9,8 @@ def tablero(t):
 
 def triqui(t):
     c = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    for a, b, c in c:
+        if t[a] == t[b] == t[c] != "":
+            return True
+    return False
+
