63b2a7ab9128328e9b98725050111c6645d66973
mariana296 <mariana2906garcia@gmail.com>
2025-01-26 21:20:37 -0500

    Version 2: Asignar los espacios

--- OneDrive/Documentos/triqui/Triqui.py
+++ OneDrive/Documentos/triqui/Triqui.py
@@ -1,1 +1,9 @@
 def tablero(t):
+    print("")
+    print(t[0] if t[0] else " ", "|", t[1] if t[1] else " ", "|", t[2] if t[2] else " ")
+    print("--+---+--")
+    print(t[3] if t[3] else " ", "|", t[4] if t[4] else " ", "|", t[5] if t[5] else " ")
+    print("--+---+--")
+    print(t[6] if t[6] else " ", "|", t[7] if t[7] else " ", "|", t[8] if t[8] else " ")
+    print("")
+
